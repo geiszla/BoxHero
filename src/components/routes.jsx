9dbe8a24562b5c6dedd0604e5a084e34d460a39d
András Geiszl <geiszla@gmail.com>
2017-03-23 01:16:10 +0000

    Added accidentally unstaged file

--- src/components/routes.jsx
+++ src/components/routes.jsx
@@ -8,41 +8,30 @@ import Ranking from './Ranking.jsx';
 import React from 'react';
 import Rules from './Rules.jsx';
 import Signup from './Signup.jsx';
-import sendQuery from '../graphql.js';
 
-export const getRoutes = (isServerSide, isLoggedIn) => {
-  const handleEnter = (nextState, replace, callback) => {
-    if (!isServerSide) {
-      sendQuery(`isLoggedIn`, false, (data) => {
-        redirect(data.isLoggedIn, nextState, replace, callback);
-      });
-    } else {
-      redirect(isLoggedIn, nextState, replace, callback);
+export const getRoutes = (isLoggedIn) => {
+  const handleEnter = (nextState, replace) => {
+    if (isLoggedIn !== undefined) {
+      const pathname = nextState.location.pathname;
+
+      if (isLoggedIn && pathname === '/signup') {
+        replace('/');
+      } else if (!isLoggedIn && pathname !== '/signup') {
+        replace('/signup');
+      }
     }
-  };
-
-  const redirect = (loggedIn, nextState, replace, callback) => {
-    const pathname = nextState.location.pathname;
-
-    if (loggedIn && pathname === '/singup') {
-      replace('/signup');
-    } else if (!loggedIn && pathname !== '/signup') {
-      replace('/');
-    }
-
-    callback();
   };
 
   return (
-    <Route component={App} onEnter={handleEnter}>
-      <Route path='/signup' component={Signup} />
-      <Route path='/' component={Core} >
+    <Route path='/' component={App} onEnter={handleEnter}>
+      <Route component={Core} >
         <IndexRoute component={Home} />
         <Route path='home' component={Home} />
         <Route path='about' component={About} />
         <Route path='rules' component={Rules} />
         <Route path='ranking' component={Ranking} />
       </Route>
+      <Route path='/signup' component={Signup} />
     </Route>
   );
 };
